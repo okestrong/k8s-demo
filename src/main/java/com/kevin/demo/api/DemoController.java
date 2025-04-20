564ca8f2a8ea4b7d79070f7c8648b8fdf7c2afdb
kevin <jc.lee@okestro.com>
2025-04-21 05:25:12 +0900

    v2

--- src/main/java/com/kevin/demo/api/DemoController.java
+++ src/main/java/com/kevin/demo/api/DemoController.java
@@ -23,30 +23,6 @@ public class DemoController {
 
     @GetMapping("hello")
     public String hello() {
-        return "Hello World v1 on %s".formatted(hostname.substring(hostname.length() - 5));
-    }
-
-    @GetMapping(value = "demo", produces = "application/vnd.kevin.random+json")
-    public String random() {
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 10;
-        String generatedString = new Random().ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-        return "Random string v1 = %s".formatted(generatedString);
-    }
-
-    @GetMapping(value = "demo", produces = "application/vnd.kevin.uuid+json")
-    public String uuid() {
-        var uuid = UUID.randomUUID();
-        var buffer = ByteBuffer.allocate(16);
-        buffer.putLong(uuid.getMostSignificantBits());
-        buffer.putLong(uuid.getLeastSignificantBits());
-        String random = Base64.getEncoder().withoutPadding().encodeToString(buffer.array())
-                .replace("/", "").replace("\\+", "-");
-        return "UUID string v1 = %s".formatted(random);
+        return "Hello World v2 on %s".formatted(hostname.substring(hostname.length() - 5));
     }
 }
